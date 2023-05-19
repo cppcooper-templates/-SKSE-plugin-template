e336e1f77390ef5e84956b442e9f56473bb4e52d
Josh Cooper <cppcooper@users.noreply.github.com>
2023-05-18 22:07:07 -0700

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,94 +1,50 @@
-//#include "Animation/Animation.h"
-//#include <Events/Events.h>
-//#include <Events/EventSinks/CrosshairRefMgr.h>
-//#include "Hooks.h"
-//#include "Input/Input.h"
-//#include "Loot.h"
-//#include "Scaleform/Scaleform.h"
-//#include "LOTD/LOTD.h"
-//#include "Items/GFxItem.h"
+//#include <Hooks/Hooks.h>
 
-namespace
-{
-	void MessageHandler(SKSE::MessagingInterface::Message* a_msg)
-	{
-		switch (a_msg->type) {
-		case SKSE::MessagingInterface::kDataLoaded:
-			//Animation::AnimationManager::Install();
+namespace {
+    void MessageHandler(SKSE::MessagingInterface::Message *a_msg) {
+        switch (a_msg->type) {
+            case SKSE::MessagingInterface::kDataLoaded:
+                //Events::Register();
+                // register hooks/events/etc.
+                break;
+        }
+    }
 
-			//Events::Register();
-			//Scaleform::Register();
-
-			//Settings::LoadSettings();
-			//LOTD::LoadLists();
-			break;
-		}
-	}
-
-	void InitializeLog(const SKSE::PluginDeclaration* plugin)
-	{
+    void InitializeLog(const SKSE::PluginDeclaration *plugin) {
 #ifndef NDEBUG
+        const auto level = spdlog::level::trace;
 		auto sink = std::make_shared<spdlog::sinks::msvc_sink_mt>();
 #else
-		auto path = SKSE::log::log_directory();
-		if (!path) {
-			stl::report_and_fail("Failed to find standard logging directory"sv);
-		}
+        const auto level = spdlog::level::info;
+        auto path = SKSE::log::log_directory();
+        if (!path) {
+            stl::report_and_fail("Failed to find standard logging directory"sv);
+        }
 
-		*path /= fmt::format("{}.log", plugin->GetName());
-		auto sink = std::make_shared<spdlog::sinks::basic_file_sink_mt>(path->string(), true);
+        *path /= fmt::format("{}.log", plugin->GetName());
+        auto sink = std::make_shared<spdlog::sinks::basic_file_sink_mt>(path->string(), true);
 #endif
+        auto log = std::make_shared<spdlog::logger>("global log"s, std::move(sink));
+        log->set_level(level);
+        log->flush_on(level);
 
-#ifndef NDEBUG
-		const auto level = spdlog::level::trace;
-#else
-		const auto level = spdlog::level::info;
-#endif
-
-		auto log = std::make_shared<spdlog::logger>("global log"s, std::move(sink));
-		log->set_level(level);
-		log->flush_on(level);
-
-		spdlog::set_default_logger(std::move(log));
-		spdlog::set_pattern("%g(%#): [%^%l%$] %v"s);
-	}
+        spdlog::set_default_logger(std::move(log));
+        spdlog::set_pattern("%g(%#): [%^%l%$] %v"s);
+    }
 }
 
-/*extern "C" DLLEXPORT constinit auto SKSEPlugin_Version = []() {
-	SKSE::PluginVersionData v;
+SKSEPluginLoad(const SKSE::LoadInterface* skse) {
+    auto *plugin = SKSE::PluginDeclaration::GetSingleton();
+    auto version = plugin->GetVersion();
 
-	v.PluginVersion(Plugin::VERSION);
-	v.PluginName(Plugin::NAME);
+    InitializeLog(plugin);
+    SKSE::log::info("{} {}.b is loading...", plugin->GetName(), version);
+    SKSE::Init(skse);
+    if (!SKSE::GetMessagingInterface()->RegisterListener(MessageHandler)) {
+        return false;
+    }
 
-	v.UsesAddressLibrary(true);
-	//v.CompatibleVersions({ SKSE::RUNTIME_LATEST });
-
-	return v;
-}();
-
-EXTERN_C [[maybe_unused]] __declspec(dllexport) bool SKSEAPI SKSEPlugin_Query(const SKSE::QueryInterface*, SKSE::PluginInfo* pluginInfo)
-{
-	pluginInfo->name = SKSEPlugin_Version.pluginName;
-	pluginInfo->infoVersion = SKSE::PluginInfo::kVersion;
-	pluginInfo->version = SKSEPlugin_Version.pluginVersion;
-	return true;
-}*/
-
-SKSEPluginLoad(const SKSE::LoadInterface* skse)
-{
-	auto* plugin = SKSE::PluginDeclaration::GetSingleton();
-	auto version = plugin->GetVersion();
-
-	//InitializeLog(plugin);
-	SKSE::log::info("{} {}.b is loading...", plugin->GetName(), version);
-	SKSE::Init(skse);
-//	SKSE::AllocTrampoline(1 << 6);
-	auto message = SKSE::GetMessagingInterface();
-	if (!message->RegisterListener(MessageHandler)) {
-		return false;
-	}
-
-	//Hooks::Install();
-
-	return true;
+    //RE::PlayerControls::jumpHandler
+    return true;
 }
+
